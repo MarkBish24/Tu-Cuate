9244e78939f5d85c9f3d364d172be27eab411d99
MarkBish24 <markbish24@gmail.com>
2025-07-16 10:13:03 -0700

    updated animation

--- src/components/Pages/PageWrapper/GradingPanels/GradingPanels.jsx
+++ src/components/Pages/PageWrapper/GradingPanels/GradingPanels.jsx
@@ -100,9 +100,9 @@ export default function GradingPanels({ onContinue, onExit }) {
           <motion.div
             key={pageIndex}
             className="grading-panel"
-            initial={{ x: 100, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            exit={{ x: -100, opacity: 0 }}
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -10, opacity: 0 }}
             transition={{ duration: 0.4 }}
           >
             {renderPage()}
